4ca960f7a10c57ea421d70b10dd4b39b49037e81
Paul Csapak <office@webcascade.com>
2017-08-29 22:20:09 +0200

    rename item model

--- tests/unit/models/invoice-item-test.js
+++ tests/unit/models/invoice-item-test.js
@@ -1,6 +1,6 @@
 import { moduleForModel, test } from 'ember-qunit';
 
-moduleForModel('item', 'Unit | Model | item', {
+moduleForModel('invoice-item', 'Unit | Model | invoice item', {
   // Specify the other units that are required for this test.
   needs: []
 });
