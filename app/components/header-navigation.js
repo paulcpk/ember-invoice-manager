2488174023059f30bcb40d5cd2e9cc634658fbb3
Paul Csapak <office@webcascade.com>
2017-10-24 00:26:18 +0200

    print button working dynamically

--- app/components/header-navigation.js
+++ app/components/header-navigation.js
@@ -1,54 +1,5 @@
 import Ember from 'ember';
 
-function openPrintDialogue() {
-  Ember.$('<iframe>', {
-    name: 'myiframe',
-    class: 'printFrame',
-    src: 'http://localhost:4200/invoices/view/2'
-  })
-  .css('display', 'block')
-  .appendTo('body');
-
-  setTimeout(() => {
-    window.frames['myiframe'].focus();
-    window.frames['myiframe'].print();
-  }, 1000);
-
-
-  // setTimeout(() => { Ember.$(".printFrame").remove(); }, 1000);
-}
-
-// const { getOwner } = Ember;
-
-function printPage() {
-  // Ember.$('#printFrame')[0].contentWindow.print();
-  // const url = 'http://localhost:4200/invoices/view/2';
-  // let printView = Ember.$('<iframe id="printFrame" name="printFrame" src='+url+' style="display: block"></iframe>');
-  // printView.appendTo('body');
-
-  
-  // printView.on('load', function() { 
-    // printView[0].contentWindow.focus();
-    // printView[0].contentWindow.print();
-    // window.frames['printFrame'].focus();
-    // window.frames['printFrame'].print();
-    // printView.remove();
-  // });
-  // openPrintDialogue();
-}
-
 export default Ember.Component.extend({
-  classNames: ['header-navigation'],
-
-  didInsertElement() {
-    const url = 'http://localhost:4200/invoices/view/2';
-    Ember.$('#printFrame').attr('src', url);
-  },
-  
-  actions: {
-    printInvoice() {
-      const printFrame = Ember.$('#printFrame');
-      printFrame[0].contentWindow.print();
-    }
-  }
+  classNames: ['header-navigation']
 });
---
+++ app/components/print-button.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+const { inject } = Ember;
+
+export default Ember.Component.extend({
+  router: inject.service(),
+  
+  didInsertElement() {
+    const router = this.get('router');
+    const invoiceId = router._router.currentState.routerJsState.params['invoices.edit'].invoice_id;
+    const url = location.origin + router.urlFor('invoices.view', {id: invoiceId});
+    Ember.$('#printFrame').attr('src', url);
+  },
+  
+  actions: {
+    printInvoice() {
+      const printFrame = Ember.$('#printFrame');
+      printFrame[0].contentWindow.print();
+    }
+  }
+});
