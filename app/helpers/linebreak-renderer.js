2d7a65108a42e75c77a6c09618a1e3167d81e930
Paul Csapak <office@webcascade.com>
2017-11-10 23:11:05 +0100

    fix linebreakRenderer

--- app/helpers/linebreak-renderer.js
+++ app/helpers/linebreak-renderer.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export function linebreakRenderer([string]) {
-  return string.replace(/\n\r?/g, '<br>').htmlSafe();
+  return string ? string.replace(/\n\r?/g, '<br>').htmlSafe() : '';
 }
 
 export default Ember.Helper.helper(linebreakRenderer);
